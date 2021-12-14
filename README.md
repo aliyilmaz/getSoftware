14b251148279ed0c63a770e0927f617cc7a3f890
Ali Yılmaz <aliyilmaz.work@gmail.com>
2021-12-14 15:05:49 +0300

    files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# getSoftware
-Detecting an server software
+## What is getSoftware ?
+
+It is used to obtain the server software name on the operating system on which the project is running. **Apache**, **Microsoft ISS** and **LiteSpeed** software are supported, other server software is named **Unknown**.
+
+**Out-of-class use:**
+
+code:
+```php
+require_once('Mind.php');
+$m = new Mind();
+echo $m::aliyilmaz('getSoftware')->getSoftware();
+```
+
+**When using it in the class:**
+
+code:
+```php
+echo self::aliyilmaz('getSoftware')->getSoftware();
+```
+
+output:
+```php
+Apache
+```
+
+---
+
+### Dependencies
+This package has no dependencies.
+
+---
+
+### License
+Instructions and files in this directory are shared under the [GPL3](https://github.com/aliyilmaz/getSoftware/blob/main/LICENSE) license.
---
+++ src/getSoftware.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *
+ * @package    getSoftware
+ * @version    Release: 1.0.0
+ * @license    GPL3
+ * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
+ * @category   Detecting an server software
+ * @link       https://github.com/aliyilmaz/getSoftware
+ *
+ */
+class getSoftware
+{
+
+   /**
+     * Detecting an server software
+     * @return string
+     */
+    public function getSoftware(){
+
+        // Server software information
+        $software = $_SERVER['SERVER_SOFTWARE'];
+
+        // The software name is being determined.
+        if (stristr($software, 'apac')) return 'Apache'; 
+        if (stristr($software, 'micr')) return 'Microsoft-IIS'; 
+        if (stristr($software, 'lites')) return 'LiteSpeed'; 
+
+        // It is stated to be undetected server software.
+        return 'Unknown';
+    }
+}
